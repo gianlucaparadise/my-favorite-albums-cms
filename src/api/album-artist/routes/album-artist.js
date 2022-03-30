424ec392435b231e7c9d43b56c9eb22127b49822
gianlucaparadise <gianlucaparadise@gmail.com>
2022-03-30 06:10:15 +0200

    Add schema

---
+++ src/api/album-artist/routes/album-artist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * album-artist router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::album-artist.album-artist');
---
+++ src/api/album-artist/services/album-artist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * album-artist service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::album-artist.album-artist');
