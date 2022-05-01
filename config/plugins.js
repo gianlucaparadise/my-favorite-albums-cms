958679e8622bbb9d518b1b985196cc456fb25a6a
gianlucaparadise <gianlucaparadise@gmail.com>
2022-05-01 12:03:07 +0200

    vercel-deploy: add dependency to external plugin

--- config/plugins.js
+++ config/plugins.js
@@ -25,6 +25,10 @@ module.exports = ({ env }) => ({
   },
   "vercel-deploy": {
     enabled: true,
-    resolve: "./src/plugins/vercel-deploy",
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+    },
   },
 });
